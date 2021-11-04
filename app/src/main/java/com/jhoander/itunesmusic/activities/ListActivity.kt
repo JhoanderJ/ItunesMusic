ab91c5f2c29455a25095805243526e76e7ce6c86
Jhoander <jhoanderjtorresquintero@gmail.com>
2021-11-04 14:42:29 -0300

    $Refactor : Se cambia nombre actividad
    - Se cambia nombre de actividad por ListActivity

--- app/src/main/java/com/jhoander/itunesmusic/activities/ListActivity.kt
+++ app/src/main/java/com/jhoander/itunesmusic/activities/ListActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jhoander.itunesmusic.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
